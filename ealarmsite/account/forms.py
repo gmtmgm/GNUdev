1bda6f1a5b1c8ee5de85b8339eb26c5b7a6c0ee1
bassy3260 <49228024+bassy3260@users.noreply.github.com>
2020-11-26 23:19:25 +0900

    .

--- ealarmsite/account/forms.py
+++ ealarmsite/account/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django import forms
 
 #회원가입 제공 class
-class signUpForm(ModelForm):
+class SignupForm(ModelForm):
     password_check = forms.CharField(max_length=200, widget=forms.PasswordInput())
     field_order=['username','password','password_check']
     class Meta:
@@ -12,7 +12,7 @@ class signUpForm(ModelForm):
         fields = ['username','password','password_check']
 
 #로그인 제공 class
-class signInForm(ModelForm):
+class SigninForm(ModelForm):
     class Meta:
         model = User
         widgets = {'password':forms.PasswordInput}
